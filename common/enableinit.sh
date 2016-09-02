da1bad1fb5ce341686734f95efb4e7c95f470d45
Christopher Mirza Abraham <wariack@hotmail.com>
2016-09-02 07:25:17 -0300

    3.0.1 released

--- common/enableinit.sh
+++ common/enableinit.sh
@@ -1,80 +1,139 @@
 #!/sbin/sh
-# Created By SadiqDev
-# init.d Support & About
-# Also Installing Busybox & resolv.conf
-# LeecoDrypt-Script Addon Installer
+# Init.d enabler by ALEXNDR (_alexndr @ XDA)
 
-ir=/system/etc/install-recovery.sh
-bb=/system/bin/busybox
-xb=/system/xbin/busybox
-sb=/system/bin/awk
-sbx=/system/xbin/awk
-xbp=/system/xbin
+OUTFD=/proc/self/fd/$2
 
-if [ -e /data/local.prop ];
-then
-	/tmp/busybox rm -f /data/local.prop
+ui_print() {
+    echo -n -e "ui_print $1\n" >> $OUTFD
+    echo -n -e "ui_print\n" >> $OUTFD
+}
+
+set_perm() {
+    chown $1.$2 $4
+    chown $1:$2 $4
+    chmod $3 $4
+    if [ -z "$5" ] ; then
+        chcon u:object_r:system_file:s0 $4
+    else
+        chcon u:object_r:$5:s0 $4
+    fi
+}
+
+resolve_link() {
+    if [ -z "$1" ] || [ ! -e $1 ] ; then return 1 ; fi
+    local VAR=$1
+    while [ -h "$VAR" ] ; do
+        VAR=$(readlink $VAR)
+    done
+    echo $VAR
+}
+
+is_mounted() {
+    if [ -z "$2" ] ; then
+        cat /proc/mounts | grep $1 >/dev/null
+    else
+        cat /proc/mounts | grep $1 | grep "$2," >/dev/null
+    fi
+    return $?
+}
+
+ui_print " "
+ui_print "=========================================="
+ui_print "Init.d enabler by ALEXNDR (_alexndr @ XDA)"
+ui_print "=========================================="
+ui_print " "
+
+SYSTEM=$(resolve_link $(find /dev/block/platform -type l | grep -i -m 1 "/app$")) ||
+SYSTEM=$(resolve_link $(find /dev/block/platform -type l | grep -i -m 1 "/system$"))
+
+if (! is_mounted /system) ; then mount -o rw /system ; fi
+if (! is_mounted /system rw) ; then mount -o rw,remount /system ; fi
+if (! is_mounted /system rw) ; then mount -t ext4 -o rw $SYSTEM /system ; fi
+if (! is_mounted /system rw) ; then mount -t f2fs -o rw $SYSTEM /system ; fi
+if (! is_mounted /system rw) ; then
+    ui_print "Failed! Can't mount /system rw, aborting!"
+    ui_print " "
+    exit 1
 fi
 
-# To prevent script from not running
-# installing Busybox if Busybox not installed on your Device
-if [ ! -e $bb ] && [ ! -e $sb ] && [ ! -e $xb ] && [ ! -e $sbx ];
-then
-	/tmp/busybox cp /tmp/busybox $xb
-	/tmp/busybox chown 0.0 $xb
-	/tmp/busybox chmod 4755 $xb
-	$xb --install -s $xbp
-fi
-/tmp/busybox sleep 2
+SYSLIB=/system/lib
+cat /system/build.prop | grep "ro.product.cpu.abilist=" | grep "64" >/dev/null && SYSLIB=/system/lib64
+cat /system/build.prop | grep "ro.product.cpu.abi=" | grep "64" >/dev/null && SYSLIB=/system/lib64
+
+# These files are prefered to trigger init.d scripts (in following order, if exists):
+# /system/etc/init.*.post_boot.sh
+# /system/etc/*.post_boot.sh
+# /system/etc/init.*.boot.sh
+# /system/etc/*.boot.sh
 #
-# Create Executor init.d
-if [ ! -e $ir ];
-then
-	if [ ! -e /system/bin/sysinit ];
-	then
-		cat > $ir
-		/tmp/busybox sleep 2
-		/tmp/busybox echo "#!/system/bin/sh" >> $ir
-		/tmp/busybox echo "#" >> $ir
-		/tmp/busybox echo "busybox run-parts /system/etc/init.d/" >> $ir
-		/tmp/busybox echo "" >> $ir
-		/tmp/busybox chown 0.0 $ir
-		/tmp/busybox chmod 4755 $ir
-	else
-		sys=`/tmp/busybox grep -l init.d /system/bin/sysinit`
-		if [ $sys != "/system/bin/sysinit" ];
-		then
-		/tmp/busybox echo "/system/bin/logwrapper run-parts /system/etc/init.d" >> /system/bin/sysinit
-		/tmp/busybox echo "" >> /system/bin/sysinit
-		fi
-	fi
+# /system/bin/debuggerd is used if there is no suitable *.sh file in /system/etc
+
+BOOTFILE=$(ls /system/etc/*.sh 2>/dev/null | grep -m 1 "/init\..*\.post_boot\.sh$") ||
+BOOTFILE=$(ls /system/etc/*.sh 2>/dev/null | grep -m 1 "\.post_boot\.sh$") ||
+BOOTFILE=$(ls /system/etc/*.sh 2>/dev/null | grep -m 1 "/init\..*\.boot\.sh$") ||
+BOOTFILE=$(ls /system/etc/*.sh 2>/dev/null | grep -m 1 "\.boot\.sh$") ||
+BOOTFILE=/system/bin/debuggerd
+
+BOOTCON=$(ls -Z $BOOTFILE 2>/dev/null | grep "u:object_r" | cut -d: -f3)
+if [ -z "$BOOTCON" ] ; then
+    BOOTCON=$(LD_LIBRARY_PATH=$SYSLIB /system/bin/toolbox ls -Z $BOOTFILE 2>/dev/null | grep "u:object_r" | cut -d: -f3)
+fi
+if [ -z "$BOOTCON" ] ; then
+    BOOTCON=$(LD_LIBRARY_PATH=$SYSLIB /system/bin/toybox ls -Z $BOOTFILE 2>/dev/null | grep "u:object_r" | cut -d: -f3)
+fi
+if [ -z "$BOOTCON" ] ; then
+    BOOTCON=system_file
+fi
+
+cat $BOOTFILE | grep "^exit 0" >/dev/null && EXIT=true || EXIT=false
+
+if [ -z "$(cat $BOOTFILE | grep "Init\.d")" ] ; then
+    if [ "$BOOTFILE" = "/system/bin/debuggerd" ] ; then
+        if [ ! -f /system/bin/debuggerd_real ] ; then
+            mv -f $BOOTFILE /system/bin/debuggerd_real
+            echo "#!/system/bin/sh" > $BOOTFILE
+        else
+            sed -i '/debuggerd_real/d' $BOOTFILE
+        fi
+    else
+        mv -f $BOOTFILE "$BOOTFILE.bak"
+        cp -pf "$BOOTFILE.bak" $BOOTFILE
+        if ($EXIT) ; then sed -i '/^exit 0/d' $BOOTFILE ; fi
+        echo "" >> $BOOTFILE
+    fi
+    echo "# Init.d support" >> $BOOTFILE
+    echo 'SU="$(ls /su/bin/su 2>/dev/null || ls /system/xbin/su) -c"' >> $BOOTFILE
+    echo 'mount -o rw,remount /system && SU="" || eval "$SU mount -o rw,remount /system"' >> $BOOTFILE
+    echo 'eval "$SU chmod 777 /system/etc/init.d"' >> $BOOTFILE
+    echo 'eval "$SU chmod 777 /system/etc/init.d/*"' >> $BOOTFILE
+    echo 'eval "$SU mount -o ro,remount /system"' >> $BOOTFILE
+    echo 'ls /system/etc/init.d/* 2>/dev/null | while read xfile ; do eval "$SU /system/bin/sh $xfile" ; done' >> $BOOTFILE
+    if [ "$BOOTFILE" = "/system/bin/debuggerd" ] ; then
+        echo '/system/bin/debuggerd_real $@' >> $BOOTFILE
+        set_perm 0 2000 755 $BOOTFILE $BOOTCON
+    else
+        if ($EXIT) ; then echo "exit 0" >> $BOOTFILE ; fi
+        chcon u:object_r:$BOOTCON:s0 $BOOTFILE
+    fi
+    mkdir -p /system/etc/init.d
+    echo "#!/system/bin/sh" > /system/etc/init.d/00test
+    echo "# Init.d test" >> /system/etc/init.d/00test
+    echo 'echo "Init.d is working !!!" > /data/initd_test.log' >> /system/etc/init.d/00test
+    echo 'echo "excecuted on $(date +"%d-%m-%Y %r")" >> /data/initd_test.log' >> /system/etc/init.d/00test
+    echo "#!/system/bin/sh" > /system/etc/init.d/99SuperSUDaemon
+    echo "/system/xbin/daemonsu --auto-daemon &" >> /system/etc/init.d/99SuperSUDaemon
+    set_perm 0 0 777 /system/etc/init.d
+    set_perm 0 0 777 "/system/etc/init.d/*"
+    ui_print "Init.d has been successfully enabled"
+    ui_print "using following file run at boot:"
+    ui_print " "
+    ui_print "$BOOTFILE"
+    ui_print " "
+    ui_print "Check result in /data/initd_test.log file"
+    ui_print " "
 else
-	irx=`/tmp/busybox grep -l init.d /system/etc/install-recovery.sh`
-	if [ $irx != "/system/etc/install-recovery.sh" ];
-	then
-		/tmp/busybox echo "busybox run-parts /system/etc/init.d/" >> $ir
-		/tmp/busybox echo "" >> $ir
-	fi
-	/tmp/busybox chmod 4755 $ir
+    ui_print "Init.d is enabled already, aborting!"
+    ui_print " " # exit is not necessary
 fi
-#####
-#######
-# Create dns config ( Used Google : Strong Recomended )
-if [ -e /system/etc/resolv.conf ];
-then
-	dns=`/tmp/busybox grep -l 8.8.8.8 /system/etc/resolv.conf`
-	dns2=`/tmp/busybox grep -l 8.8.4.4 /system/etc/resolv.conf`
-	if [ $dns != "/system/etc/resolv.conf" ] && [ $dns2 != "/system/etc/resolv.conf" ];
-	then
-		/tmp/busybox echo "nameserver 8.8.8.8" >> /system/etc/resolv.conf
-		/tmp/busybox echo "nameserver 8.8.4.4" >> /system/etc/resolv.conf
-	fi
-else
-	cat > /system/etc/resolv.conf
-	/tmp/busybox echo "nameserver 8.8.8.8" >> /system/etc/resolv.conf
-	/tmp/busybox echo "nameserver 8.8.4.4" >> /system/etc/resolv.conf
-	sleep 1
-	/tmp/busybox chmod 644 /system/etc/resolv.conf
-fi
-/tmp/busybox sleep 2
-fi
+
+exit 0

a21d4f250c51801e28f3ba935f6ab95ec42532ed
F4uzan <f4uzan@this.is.f4>
2017-04-14 22:15:20 +0700

    Add buildscript for Windows
    This version is largely untested, it's a quick port over the Linux
    version.
    
    Provided with this commit is the "zip" binary for Windows, this is due
    to the fact that most zip installation for Windows are either localised
    (only for a certain program / app), to workaround the issue, we provide
    our own copy of the "zip" binary and its dependency.
    
    The copy of the binary is built by GnuWin / gnuwin32 @ sourceforge.

---
+++ build.bat
@@ -0,0 +1,29 @@
+@Echo Off
+title Gov-Tuner Buildscript
+
+:: Gov-Tuner ZIP creation script
+:: Written by F4uzan, with the help of Gov-Tuner Team
+:: zip and bzip2 binaries provided by GnuWin / gnuwin32 at http://gnuwin32.sourceforge.net
+::
+:: This script should build a zip archive containing all the files excluding the ".git" folder
+:: "zip" binary is provided in the "win" folder, this folder will also be excluded out when compiling the zip
+:: "git" binary can be separately installed and will be needed in case you're building without the "zip" arguments
+::
+:: Arguments:
+:: build <ARGUMENTS> <VERSION>
+::
+:: Additional arguments:
+:: -zip	: Use zip instead of git archive
+::
+:: Example:
+:: build 4.0.1
+
+set use_zip=%1
+set version=%2
+set zip_dir=win/zip.exe
+
+if "%use_zip%" == "-zip" (
+	start %zip_dir% -r Gov-Tuner_%version%.zip . -x ".git/*" "win/*"
+) else (
+	git archive -o Gov-Tuner_%version%.zip HEAD
+)

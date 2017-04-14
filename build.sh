c62a969f03726a3ec726398c8e3adc7795168876
F4uzan <f4uzan@this.is.f4>
2017-04-14 14:06:12 +0700

    Add a buildscript for zip generation
    The script automatically generates a zip for convenience purpose. I'll
    write the Windows version later on, but the current version should work
    on both macOS and general Linux distribution.

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+## Gov-Tuner ZIP creation
+### Written by F4uzan, with the help of Gov-Tuner Team
+#
+# This script should build a zip archive containing all the files excluding the ".git" folder
+# Works on Linux with proper "git" and "zip" installation
+#
+# Arguments:
+# ./build.sh <ARGUMENTS> <VERSION>
+#
+# Additional arguments
+# -zip	: Use zip instead of git archive
+#
+# Example:
+# ./build.sh 4.0.1
+
+version=$2
+
+if [ $1 == "-zip" ]; then
+	zip -r Gov-Tuner_$version.zip . -x *.git*
+else
+	git archive -o Gov-Tuner_$version.zip HEAD
+fi

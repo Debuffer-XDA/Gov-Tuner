075d09bd165a8a4b74c5b82d868324d94c79d0b8
F4uzan <f4uzan@this.is.f4>
2017-04-14 17:21:00 +0700

    Nuke "git" from buildscript
    Just use "zip" to simplify the whole code.

--- build.bat
+++ build.bat
@@ -7,19 +7,14 @@ title Gov-Tuner Buildscript
 ::
 :: This script should build a zip archive containing all the files excluding the ".git" folder
 :: "zip" binary is provided in the "win" folder, this folder will also be excluded out when compiling the zip
-:: "git" binary can be separately installed and will be needed in case you're building without the "zip" arguments
 ::
 :: Arguments:
-:: build <ARGUMENTS> <VERSION>
-::
-:: Additional arguments:
-:: -zip	: Use zip instead of git archive
+:: build <VERSION>
 ::
 :: Example:
 :: build 4.0.1
 
-set use_zip=%1
-set version=%2
+set version=%1
 set zip_dir=win
 set zip_exec=zip.exe
 set dir=%cd%
@@ -35,14 +30,7 @@ echo Moving Uninstaller to common\system\etc\GovTuner
 move /y Uninstall_Gov-Tuner.zip ../common/system/etc/GovTuner>nul
 cd ..
 
-if "%use_zip%" == "-zip" (
-	echo Using zip to build output
-	echo Building output zip
-	%zip_dir%\%zip_exec% -r Gov-Tuner_%version%.zip . -x ".git/*" "win/*" "uninstaller/*" "build.*" ".gitignore">nul
-	echo Output created: %dir%\Gov-Tuner_%version%.zip
-) else (
-	echo Using git to build output
-	echo Building output zip
-	git archive -o Gov-Tuner_%version%.zip HEAD
-	echo Output created: %dir%\Gov-Tuner_%version%.zip
-)
+echo Using zip to build output
+echo Building output zip
+%zip_dir%\%zip_exec% -r Gov-Tuner_%version%.zip . -x ".git/*" "win/*" "uninstaller/*" "build.*" ".gitignore">nul
+echo Output created: %dir%\Gov-Tuner_%version%.zip
--- build.sh
+++ build.sh
@@ -3,18 +3,15 @@
 ### Written by F4uzan, with the help of Gov-Tuner Team
 #
 # This script should build a zip archive containing all the files excluding the ".git" folder
-# Works on Linux with proper "git" and "zip" installation
+# Works on Linux with proper "zip" installation
 #
 # Arguments:
-# ./build.sh <ARGUMENTS> <VERSION>
-#
-# Additional arguments
-# -zip	: Use zip instead of git archive
+# ./build.sh <VERSION>
 #
 # Example:
 # ./build.sh 4.0.1
 
-version=$2
+version=$1
 dir=$(pwd)
 
 echo ""
@@ -28,15 +25,8 @@ echo "Moving Uninstaller to common/system/etc/GovTuner"
 mv Uninstall_Gov-Tuner.zip ../common/system/etc/GovTuner
 cd ..
 
-if [ $1 == "-zip" ]; then
-	echo "Using zip to build output"
-	echo "Building output zip"
-	zip -r Gov-Tuner_$version.zip . -x ".git/*" "win/*" "uninstaller/*" "build.*" ".gitignore">/dev/null
-	echo "Output created: $dir/Gov-Tuner_$version.zip"
-else
-	echo "Using git to build output"
-	echo "Building output zip"
-	git archive -o Gov-Tuner_$version.zip HEAD
-	echo "Output created: $dir/Gov-Tuner_$version.zip"
-fi
+echo "Using zip to build output"
+echo "Building output zip"
+zip -r Gov-Tuner_$version.zip . -x ".git/*" "win/*" "uninstaller/*" "build.*" ".gitignore">/dev/null
+echo "Output created: $dir/Gov-Tuner_$version.zip"
 echo ""

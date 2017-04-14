ed5e8d7974eae3dec209003743760cd3355b9852
F4uzan <f4uzan@this.is.f4>
2017-04-14 16:56:14 +0700

    Properly verbose buildscript output
    This gives a better output to the user, instead of barraging them with
    output from zip

--- build.bat
+++ build.bat
@@ -22,15 +22,27 @@ set use_zip=%1
 set version=%2
 set zip_dir=win
 set zip_exec=zip.exe
+set dir=%cd%
 
+echo:
+echo Building Gov-Tuner v%version%
+echo -----------------------------
 :: Build and copy uninstaller before doing anything
+echo Building Uninstaller
 cd uninstaller
-..\%zip_dir%\%zip_exec% -r Uninstall_Gov-Tuner.zip .
-move /y Uninstall_Gov-Tuner.zip ../common/system/etc/GovTuner
+..\%zip_dir%\%zip_exec% -r Uninstall_Gov-Tuner.zip .>nul
+echo Moving Uninstaller to common/system/etc/GovTuner
+move /y Uninstall_Gov-Tuner.zip ../common/system/etc/GovTuner>nul
 cd ..
 
 if "%use_zip%" == "-zip" (
-	start %zip_dir%\%zip_exec% -r Gov-Tuner_%version%.zip . -x ".git/*" "win/*" "uninstaller/*" "build.*"
+	echo Using zip to build output
+	echo Building output zip
+	%zip_dir%\%zip_exec% -r Gov-Tuner_%version%.zip . -x ".git/*" "win/*" "uninstaller/*" "build.*">nul
+	echo Output created: %dir%/Gov-Tuner_%version%.zip
 ) else (
+	echo Using git to build output
+	echo Building output zip
 	git archive -o Gov-Tuner_%version%.zip HEAD
+	echo Output created: %dir%/Gov-Tuner_%version%.zip
 )
--- build.sh
+++ build.sh
@@ -15,14 +15,28 @@
 # ./build.sh 4.0.1
 
 version=$2
+dir=$(pwd)
+
+echo ""
+echo "Building Gov-Tuner v$2"
+echo "----------------------"
 
 # Build and copy uninstaller before doing anything
-cd uninstaller; zip -r Uninstall_Gov-Tuner.zip .
+echo "Building Uninstaller"
+cd uninstaller; zip -r Uninstall_Gov-Tuner.zip .>/dev/null
+echo "Moving Uninstaller to common/system/etc/GovTuner"
 mv Uninstall_Gov-Tuner.zip ../common/system/etc/GovTuner
 cd ..
 
 if [ $1 == "-zip" ]; then
-	zip -r Gov-Tuner_$version.zip . -x ".git/*" "win/*" "uninstaller/*" "build.*"
+	echo "Using zip to build output"
+	echo "Building output zip"
+	zip -r Gov-Tuner_$version.zip . -x ".git/*" "win/*" "uninstaller/*" "build.*">/dev/null
+	echo "Output created: $dir/Gov-Tuner_$version.zip"
 else
+	echo "Using git to build output"
+	echo "Building output zip"
 	git archive -o Gov-Tuner_$version.zip HEAD
+	echo "Output created: $dir/Gov-Tuner_$version.zip"
 fi
+echo ""

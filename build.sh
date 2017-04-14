881ae36b35f95682ca8b7bcd5082576143618291
F4uzan <f4uzan@this.is.f4>
2017-04-14 15:18:46 +0700

    Fix zip arguments on macOS
    *.git* didn't work as well as expected, change this to ".git/*" so that
    it can work better on macOS.

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@
 version=$2
 
 if [ $1 == "-zip" ]; then
-	zip -r Gov-Tuner_$version.zip . -x *.git*
+	zip -r Gov-Tuner_$version.zip . -x ".git/*"
 else
 	git archive -o Gov-Tuner_$version.zip HEAD
 fi
